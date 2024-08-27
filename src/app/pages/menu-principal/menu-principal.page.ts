5415dcf2b4e0a6a77403a6dcc068794521df470d
Dondup <do.berrios@duocuc.cl>
2024-08-26 23:36:06 -0400

    7/10 del menu principal

--- src/app/pages/menu-principal/menu-principal.page.ts
+++ src/app/pages/menu-principal/menu-principal.page.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuPrincipalPage implements OnInit {
 
+  alarmas: any = [
+    {
+      medicamento: "Apralozam",
+      dosis: "200 mg",
+      hora: "14:30"
+    },
+
+    {
+      medicamento: "Cardevilol",
+      dosis: "12.5 mg",
+      hora: "22:00"
+    },
+
+    {
+      medicamento: "Losartan",
+      dosis: "50 mg",
+      hora: "18:45"
+    },
+
+  ];
+
   constructor() { }
 
   ngOnInit() {

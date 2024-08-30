8483d3bc105595ad2d185abf965cc04f1a222c5f
Dondup <do.berrios@duocuc.cl>
2024-08-30 04:51:28 -0400

    Termino de avances contacto emergencia

--- src/app/pages/autocuidado/perfil-emergencia/perfil-emergencia.page.ts
+++ src/app/pages/autocuidado/perfil-emergencia/perfil-emergencia.page.ts
@@ -10,13 +10,29 @@ export class PerfilEmergenciaPage implements OnInit {
   myVar:boolean=true;
   campos:any = [
     {
-      nombre: "Chad",
-      apellidoP: "Giga",
-      apellidoM: "Gonzalez",
-      correo: "perfectman@gmail.com",
-      telefono: 912345678,
-      comuna: "Conchali"
+      label_input: "nombre",
+      dato_usuario: "Jorge"
     },
+    {
+      label_input: "Apellido paterno",
+      dato_usuario: "Gonzalez"
+    },
+    {
+      label_input: "Apellido materno",
+      dato_usuario: "Jalero"
+    },
+    {
+      label_input: "Correo electronico",
+      dato_usuario: "JorgeGonJales@gmail.com"
+    },
+    {
+      label_input: "Telefono",
+      dato_usuario: 912345678
+    },
+    {
+      label_input: "Comuna de residencia",
+      dato_usuario: "Conchali"
+    }
   ]
 
   constructor() { }

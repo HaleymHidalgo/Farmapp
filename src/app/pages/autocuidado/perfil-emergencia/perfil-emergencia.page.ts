c890c3147e59484bd4ff614dd088eb4a92d06103
Dondup <do.berrios@duocuc.cl>
2024-08-30 01:25:06 -0400

    Toggle de 1/2 perfiles listo

--- src/app/pages/autocuidado/perfil-emergencia/perfil-emergencia.page.ts
+++ src/app/pages/autocuidado/perfil-emergencia/perfil-emergencia.page.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilEmergenciaPage implements OnInit {
 
+  myVar:boolean=true;
+  campos:any = [
+    {
+      nombre: "Chad",
+      apellidoP: "Giga",
+      apellidoM: "Gonzalez",
+      correo: "perfectman@gmail.com",
+      telefono: 912345678,
+      comuna: "Conchali"
+    },
+  ]
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  editarCampos() {
+    this.myVar=!this.myVar;
   }
 
 }

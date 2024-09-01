e760eed21f84fe7ce319b501127c7b81b8012ff3
HaleymHidalgo <haleymgabrielhidalgo@gmail.com>
2024-08-31 22:02:44 -0400

    Añadida pagina de pregunta-seguridad + validaciones + router

--- src/app/pages/soporte/pregunta-seguridad/pregunta-seguridad.page.ts
+++ src/app/pages/soporte/pregunta-seguridad/pregunta-seguridad.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-pregunta-seguridad',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pregunta-seguridad.page.scss'],
 })
 export class PreguntaSeguridadPage implements OnInit {
+  //variable que almacena la respuesta a la pregunta de seguridad
+  respuesta!: string;
+  respuestaCorrecta: string = "alan";
 
-  constructor() { }
+  constructor(private alertcontroller: AlertController, private router: Router) { }
 
   ngOnInit() {
   }
 
+  validarRespuesta(){
+    //Validamos que el campo no este vacio
+    if(this.respuesta == undefined || this.respuesta == ""){
+      const titulo = "Campos vacios";
+      const mensaje = "Por favor ingrese alguna respuesta en el campo de respuesta";
+      this.alerta(titulo, mensaje);
+      return;
+    }
+
+    //Validamos que la respuesta sea correcta
+    if(this.respuesta.toLowerCase() == this.respuestaCorrecta){
+      this.router.navigate(['/soporte/opciones-cliente']);
+    }
+  }
+
+
+  async alerta(titulo:string , mensaje: string){
+    const alert = await this.alertcontroller.create({
+      header: titulo,
+      message: mensaje,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
 }
